c8df1fc63016a118503cd63b7482b48c4816f833
RomaricVandycke <145856813+RomaricVandycke@users.noreply.github.com>
2023-12-05 15:20:22 +0100

    Add files via upload

---
+++ BaseLine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#module reset
+#module load miniconda3
+#conda activate mytorch
+#source $STORE/mytorchdist/bin/activate
+#conda activate mytorchdist
+#source $STORE/conda/envs/mytorchdist/bin/activate
+which python
+python BaseLine.py 
